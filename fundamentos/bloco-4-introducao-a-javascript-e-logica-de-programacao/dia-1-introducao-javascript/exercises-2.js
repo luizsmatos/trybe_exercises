5232897c0e2e218443e4dda8b5531403836abfb1
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-23 15:13:43 -0300

    Exercises 1 - Conclusao do exercises-2.js - Comparar duas variaveis

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercises-2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercises-2.js
@@ -1,2 +1,13 @@
-let a = ;
-let b = ;
+let a = 15;
+let b = 16;
+
+// let resultado = Math.max(a, b);
+
+// console.log(resultado);
+
+if (a > b) {
+  console.log("'a' e maior que 'b'");
+}
+else {
+  console.log("'b' e maior que 'a'")
+}
