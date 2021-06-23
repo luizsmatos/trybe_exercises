d219117fb7c7a0d0972d27f744dec55f6d223ef0
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-23 15:26:56 -0300

    Exercises 1 - Conclusao exercises-4.js - Verificar o estado de uma variavel

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercises-4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercises-4.js
@@ -1,2 +1,12 @@
-let a = ;
-let b = ;
+let a = 0;
+
+
+if (a > 0) {
+  console.log("Esse numero e positivo")
+}
+else if ( a < 0) {
+  console.log("Esse numero e negativo")
+}
+else {
+  console.log("Esse numero e igual a 0")
+}
