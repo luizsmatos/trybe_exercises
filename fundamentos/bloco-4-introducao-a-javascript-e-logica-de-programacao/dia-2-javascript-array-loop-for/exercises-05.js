85edb8ec960320dca2a7cae5ef9a8a0ba8b7e2ec
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-24 18:39:05 -0300

    Introduzido enunciado dos exercicios, dentro dos respectivos.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-05.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-05.js
@@ -1,3 +1,7 @@
+
+// Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let max = 0;
 
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-09.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-09.js
@@ -1,3 +1,7 @@
+
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+
 let number = [];
 
 
