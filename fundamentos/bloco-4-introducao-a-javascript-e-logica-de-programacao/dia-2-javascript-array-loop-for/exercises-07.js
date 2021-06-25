5e15f950ccd11d433e89696665ee9b432c0e867f
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-25 18:06:55 -0300

    Correcao de variaveis

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-07.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-07.js
@@ -2,10 +2,10 @@
 // Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let max = 0;
+let min = 0;
 
 for (let index = 0; index < numbers.length; index += 1){
-  max = Math.min(...numbers);
+    min = Math.min(...numbers);
 
 }
 console.log(max);
