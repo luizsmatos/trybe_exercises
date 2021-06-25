46f79178019ace82ace123e41dae6d22a1de8cda
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-24 21:44:48 -0300

    Modificacoes em declaracao de variaveis

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-08.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-08.js
@@ -3,7 +3,7 @@
 
 let number = [];
 
-for ( let index = 1; index <= 25; index += 1){
+for (let index = 1; index <= 25; index += 1){
   number.push(index);
 }
 
--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-09.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-loop-for/exercises-09.js
@@ -5,11 +5,11 @@
 let number = [];
 
 
-for ( index = 1; index <= 25; index += 1){
+for (let index = 1; index <= 25; index += 1){
   number.push(index);
 }
 
-for (index = 0; index < number.length; index += 1){
+for (let index = 0; index < number.length; index += 1){
   let divNumber = number[index] / 2;
   console.log(divNumber);
 }
