f34c104d6a4f1d385e13b69450f73b1afed9fe5e
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-25 18:57:12 -0300

    Exercises 1 - imprima na tela um quadrado feito de asteriscos de lado de tamanho n.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercises-001.js
@@ -0,0 +1,10 @@
+let n = 5;
+
+for (let index = 1; index <= n; index += 1){
+  let coluna = "";
+  for (let sIndex = 1; sIndex <= n; sIndex += 1){
+  coluna += "*";
+  
+  } 
+  console.log(coluna)
+}
