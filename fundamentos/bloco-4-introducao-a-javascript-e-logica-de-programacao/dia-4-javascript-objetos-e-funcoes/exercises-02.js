fd50156c488561128845710d2ac5e8d29237fea3
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-28 19:39:49 -0300

    Conclusao Exercises - Parte 2 - Exercises 03

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises-02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises-02.js
@@ -27,17 +27,31 @@
 //  Array de teste: [2, 3, 6, 7, 10, 1]; .
 //  Valor esperado no retorno da função: 4 .
 
-function indiceDoMaior(numeros) {
-  let indiceMaior = 0;
-  for (let indice in numeros) {
-    if (numeros[indiceMaior] < numeros[indice]) {
-      indiceMaior = indice;
-    }
-  }
-  return indiceMaior;
-}
+// function indiceDoMaior(numeros) {
+//   let indiceMaior = 0;
+//   for (let indice in numeros) {
+//     if (numeros[indiceMaior] < numeros[indice]) {
+//       indiceMaior = indice;
+//     }
+//   }
+//   return indiceMaior;
+// }
 
-console.log(indiceDoMaior([2, 3, 6, 7, 10, 1])); //4
+// console.log(indiceDoMaior([2, 3, 6, 7, 10, 1])); //4
 
 
   
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+//  Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+//  Valor esperado no retorno da função: 6 .
+
+// function indiceDoMenor(numeros){
+//   let indiceMenor = 0;
+//   for (let index in numeros){
+//     if (numeros[index] < numeros[indiceMenor])
+//     indiceMenor = index
+//   }
+//  return indiceMenor
+// }
+
+// console.log(indiceDoMenor([2, 4, 6, 7, 10, 3, 8]));
