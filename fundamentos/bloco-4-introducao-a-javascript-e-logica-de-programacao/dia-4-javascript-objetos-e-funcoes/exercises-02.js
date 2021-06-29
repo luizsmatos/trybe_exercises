8fca9eb5cde65038e5429307fcc3beafa4e200b2
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-29 08:42:52 -0300

    Conclusao Exercises - Parte 2 - Exercises 04

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises-02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercises-02.js
@@ -42,6 +42,7 @@
 
   
 // 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
 //  Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
 //  Valor esperado no retorno da função: 6 .
 
@@ -55,3 +56,22 @@
 // }
 
 // console.log(indiceDoMenor([2, 4, 6, 7, 10, 3, 8]));
+
+
+
+//  4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+//  Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
+//  Valor esperado no retorno da função: Fernanda .
+
+function array(word){
+  let maiorPalavra = word[0];
+    for (let index in word){
+      if (maiorPalavra.length < word[index].length){
+      maiorPalavra = word[index];
+      }
+    }
+  return maiorPalavra;
+}
+
+console.log(array(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
