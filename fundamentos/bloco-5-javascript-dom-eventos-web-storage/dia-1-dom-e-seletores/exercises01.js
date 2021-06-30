8c8b4be237db3ca82f6063138fa40e8fc59389c3
Luiz Gustavo <gusstavopdm@gmail.com>
2021-06-30 18:30:11 -0300

    Conclusao Exercises - Bloco-5-dia-1.

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-1-dom-e-seletores/exercises01.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-1-dom-e-seletores/exercises01.js
@@ -10,3 +10,41 @@
  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
         */
+
+
+function changeText() {
+  let paragraphOne = document.getElementsByTagName("p")[0];
+  paragraphOne.innerHTML = "Espero que esteja consolidado na area de desenvolvimente web, vivenciando varios desafios!";
+}
+changeText()
+
+function changeColorGreen() {
+  let bgColor = document.getElementsByClassName("main-content")[0];
+    bgColor.style.backgroundColor = "rgb(76,164,109)";
+}
+changeColorGreen()
+
+function changeColorWhite() {
+  let bgColor = document.getElementsByClassName("center-content")[0];
+    bgColor.style.backgroundColor = "white";
+}
+changeColorWhite()
+
+function changeTitle() {
+  let title = document.getElementsByClassName("title")[0];
+  title.innerHTML = "Exercicio 5.1 - JavaScript"
+}
+changeTitle()
+
+function upperCase(){
+  let text = document.getElementsByTagName("p")[0];
+  text.style.textTransform = "uppercase";
+}
+upperCase()
+
+function viewParagraph() {
+  let paragraph = document.getElementsByTagName("p");
+  for (let index = 0; index < paragraph.length; index += 1)
+  console.log(paragraph[index].innerHTML);
+}
+viewParagraph()
