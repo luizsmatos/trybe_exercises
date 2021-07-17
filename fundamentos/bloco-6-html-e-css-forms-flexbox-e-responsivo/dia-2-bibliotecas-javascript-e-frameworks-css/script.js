adabca7a9dfe5756cfaf0f1f405387d3ac503e26
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-16 21:56:09 -0300

    Retirado biblioteca Just-validade, para validacao dos campos & Instalado biblioteca Popup-validation.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -1,6 +1,7 @@
 const province = document.getElementById("province");
 const buttonSubmit = document.getElementById("submit");
-const form = document.forms[0].elements;
+const form = document.querySelector('.js-form')
+const formElements = document.forms[0].elements;
 const inputCpf = document.getElementById("cpf-number");
 
 // Creditos pelo Array, colega de turma @Michael
@@ -101,7 +102,7 @@ function formataCPF() {
 
 let pickerStart = new Pikaday({
   field: document.getElementById("datepicker"),
-  format: "DD/M/YYYY",
+  format: "DD/MM/YYYY",
   minDate: new Date(1960, 0, 1),
   maxDate: new Date(),
   toString(date, format) {
@@ -124,7 +125,7 @@ let pickerStart = new Pikaday({
 
 let pickerEnd = new Pikaday({
   field: document.getElementById("datepicker2"),
-  format: "DD/M/YYYY",
+  format: "DD/MM/YYYY",
   minDate: new Date(1960, 0, 1),
   maxDate: new Date(),
   toString(date, format) {
@@ -147,68 +148,90 @@ let pickerEnd = new Pikaday({
 
 // Personalize as regras de validação padrão.
 
-new window.JustValidate(form, {
-  Rules: {
-    firstname: {
-      required: true,
-      minLength: 3,
-      maxLength: 15,
-    },
-    lastname: {
-      required: true,
-      minLength: 3,
-      maxLength: 15,
-    },
-    email: {
-      required: true,
-      email: true,
-    },
-    complement: {
-      required: true,
-    },
-    text: {
-      required: true,
-      maxLength: 300,
-      minLength: 5,
-    },
-    zip: {
-      required: true,
-      zip: true,
-    },
-  },
-});
 
-// Costumizar as mensagens.
 
-new window.JustValidate(form, {
-  Messages: {
-    required: "O campo é obrigatório",
-    email: "Por favor, digite um email válido",
-    maxLength: "O campo deve conter no máximo: caracteres de valor",
-    minLength: "O campo deve conter no mínimo: caracteres de valor",
-    password: "Senha não é válida",
-    remote: "E-mail já existe",
-  },
-});
 
-// Defina a cor de suas mensagens de erro:
+//   rules: {
+//     firstname: {
+//       required: true,
+//       minLength: 3,
+//       maxLength: 40,
+//     },
+//     lastname: {
+//       required: true,
+//       minLength: 3,
+//       maxLength: 40,
+//     },
+//     email: {
+//       required: true,
+//       email: true,
+//       maxLength: 50,
+//     },
+//     cpf: {
+//       required: true,
+//       minLength: 11,
+//       maxLength: 11,
+//     },    
+//     address: {
+//       required: true,
+//       minLength: 3,
+//       maxLength: 200,
+//     },
+//     city: {
+//       required: true,
+//       minLength: 3,
+//       maxLength: 28,
+//     },
+//     province: {
+//       type: 'selected',
+//       required: true,
+//     },
+//     complement: {
+//       type: 'radio',
+//       required: true,
+//     },
+//     cep: {
+//       required: true,
+//       cep: true,
+//     },
+//     resume: {
+//       required: true,
+//       maxLength: 1000,
+//       minLength: 150,
+//     },
+//     job: {
+//       required: true,
+//       maxLength: 40,
+//       minLength: 5,
+//     },
+//     discjob: {
+//       required: true,
+//       maxLength: 300,
+//       minLength: 5,
+//     },
+//     dateStart: {
+//       required: true,
+//     },
+//     dateEnd: {
+//       required: true,
+//     },
+//   },
+// });
 
-new window.JustValidate(form, {
-  colorWrong: "#B81111",
-});
 
 // Criacao das respostas em uma Div.
 
-buttonSubmit.addEventListener("click", (event) => {
-  // event.preventDefault();
-  const newDiv = document.getElementById("values");
+// buttonSubmit.addEventListener("click", (event) => {
+//   // event.preventDefault();
+//   const newDiv = document.getElementById("values");
 
-  for (let index = 0; index < form.length; index += 1) {
-    const inputsValue = form[index].value;
-    const inputsName = form[index].name;
-    let keyValue = `${inputsName} = ${inputsValue}`;
-    const newParagraph = document.createElement("p");
-    newParagraph.innerHTML = keyValue;
-    newDiv.appendChild(newParagraph);
-  }
-});
+//   for (let index = 0; index < form.length; index += 1) {
+//     const inputsValue = formElements[index].value;
+//     const inputsName = formElements[index].name;
+//     let keyValue = `${inputsName} = ${inputsValue}`;
+//     const newParagraph = document.createElement("p");
+//     newParagraph.innerHTML = keyValue;
+//     newDiv.appendChild(newParagraph);
+//   }
+// });
+
