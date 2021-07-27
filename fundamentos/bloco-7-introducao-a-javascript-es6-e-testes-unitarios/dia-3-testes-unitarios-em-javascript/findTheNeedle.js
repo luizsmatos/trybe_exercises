b949e9bfbd3cb7c8ebc8ed22499c37d1b29b3258
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-27 08:39:55 -0300

    Exercicio 4 TDD/Part 2 - Function findTheNeedle

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/findTheNeedle.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+// Escreva a função findTheNeedle para passar nos testes já implementados.
+
+function findTheNeedle (array, word) {
+  let wordOfIndex = -1;
+
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === word) wordOfIndex = index;
+  }
+  return wordOfIndex;
+}
+
+
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
