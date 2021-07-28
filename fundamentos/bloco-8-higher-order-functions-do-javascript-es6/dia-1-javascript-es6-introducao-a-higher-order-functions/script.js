fa1302c3de9af3d12d1a25cef4c6eb25971ea72f
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-28 20:01:43 -0300

    Exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,6 +1,10 @@
+// 1 -  Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
+
+
 const employeeGenerator = (fullName) => {
   const email = fullName.toLowerCase().split(' ').join('_');
-  return { fullName, email: `${email}@trybe.com` };
+  return { fullName, 
+    email: `${email}@trybe.com` };
 };
 
 const newEmployees = (callback) => {
@@ -13,3 +17,24 @@ const newEmployees = (callback) => {
 };
 
 console.log(newEmployees(employeeGenerator));
+
+// 2 - Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const generatorNumber = (num, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return callback(num, number) ? console.log("Parabéns você ganhou") : console.log("Tente novamente");
+}
+
+const result = (num, sorteado) => {
+
+  console.log(`O numero sorteado: ${sorteado}`)
+  return num === sorteado;
+}
+
+generatorNumber(1, result);
+
+
+
+
+
