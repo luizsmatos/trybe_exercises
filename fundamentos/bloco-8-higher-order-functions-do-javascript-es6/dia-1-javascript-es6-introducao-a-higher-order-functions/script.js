820f30cf85f118705cc02c1bb6d5288f26259d39
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-29 08:34:01 -0300

    Exercicio 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -34,7 +34,29 @@ const result = (num, sorteado) => {
 
 generatorNumber(1, result);
 
+// 3 - Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. 
+// Ao final a HOF deve retornar o total da contagem de respostas certas.
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
 
 
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 
+const compararNotas = (rightAnswers, studentAnswers, callback) => {
+  let contador = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = callback(rightAnswers[index], studentAnswers[index]);
+    contador += actionReturn;
+  }
+  return `Resultado final: ${contador} corretas`;
+};
+
+console.log(compararNotas(RIGHT_ANSWERS, STUDENT_ANSWERS, (rAnswer, uAnswer) => {
+  if (rAnswer === uAnswer) {
+    return 1;
+  } if (uAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+}))
