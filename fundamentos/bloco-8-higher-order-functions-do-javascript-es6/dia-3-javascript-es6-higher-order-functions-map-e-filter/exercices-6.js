3d83ddcb835134b6699a1d03bf5641f4b8f70598
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-30 19:22:36 -0300

    Conclusao Exercicios 6

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercices-6.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercices-6.js
@@ -4,3 +4,20 @@ const books = require('./data');
 
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+  'O Senhor dos Anéis',
+  'Fundação',
+  'O Chamado de Cthulhu',
+];
+
+function oldBooks() {
+  // escreva seu código aqui
+  return books
+  .filter((book) => ( 2021 - book.releaseYear) > 60)
+  .map((nameBook) => nameBook.name) 
+}
+
+
+assert.deepStrictEqual(oldBooks(), expectedResult);
+
