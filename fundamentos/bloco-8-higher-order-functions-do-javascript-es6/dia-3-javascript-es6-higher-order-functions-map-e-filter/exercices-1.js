3f7f0a0cc25eadae8793f4fbef218d6e248c043f
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-30 15:05:56 -0300

    Conclusao Exercicios 1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercices-1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercices-1.js
@@ -65,3 +65,20 @@ const books = [
 
 
 // Adicione o código do exercício aqui:
+
+// 1 
+
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formatedBookNames() {
+  // escreva seu código aqui
+  return books.map((names) => `${names.name} - ${names.genre} - ${names.author.name}`)
+}
+assert.deepStrictEqual(formatedBookNames(), expectedResult);
