b8ffa10267e71f8e3cdc855739d9c14e016fecaa
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-30 16:27:55 -0300

    Conclusao Exercicios 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercices-5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercices-5.js
@@ -65,3 +65,21 @@ const books = [
 
 
 // Adicione o código do exercício aqui:
+
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+function fantasyOrScienceFictionAuthors(genre1, genre2) {
+  // escreva seu código aqui
+  return books
+  .filter((nameGenre) => nameGenre.genre === genre1 || nameGenre.genre === genre2)
+  .map((nameAuthor) => nameAuthor.author.name).sort()
+}
+
+// console.log(fantasyOrScienceFictionAuthors('Ficção Científica', 'Fantasia'))
+
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors('Ficção Científica', 'Fantasia'), expectedResult);
