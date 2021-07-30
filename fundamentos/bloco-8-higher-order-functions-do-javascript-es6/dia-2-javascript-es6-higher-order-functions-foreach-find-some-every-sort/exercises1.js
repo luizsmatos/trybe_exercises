1f82d2f9e1a2028772d0d19deb64907e82ec54d6
Luiz Gustavo <gusstavopdm@gmail.com>
2021-07-30 09:11:49 -0300

    Exercicios 3 a 7

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercises1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercises1.js
@@ -77,16 +77,128 @@ assert.strictEqual(authorBornIn1947(), 'Stephen King');
 // 2 
 
 function smallerName() {
-  let nameBook;
+  let author;
   // escreva aqui o seu código
 
   books.forEach((book) => {
-    if (!nameBook || book.name.length < nameBook.length) {
-      nameBook = book.name;
+    if (!author || book.name.length < author.length) {
+      author = book.name;
     }
   });
-  // Variável nameBook que receberá o valor do menor nome;
-  return nameBook;
+  // Variável author que receberá o valor do menor nome;
+  return author;
 }
 
 assert.strictEqual(smallerName(), 'Duna');
+
+// 3 
+
+const expectedResult1 = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return books.find((book) => book.name.length === 26);
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult1);
+
+
+// 4
+
+const expectedResult2 = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947 },
+    releaseYear: 1986,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+    releaseYear: 1928,
+  },
+];
+
+function booksOrderedByReleaseYearDesc() {
+  return books.sort((bookA, bookB) => bookB.releaseYear - bookA.releaseYear);
+}
+
+assert.deepStrictEqual(booksOrderedByReleaseYearDesc(), expectedResult2);
+
+
+// 5
+
+const expectedResult3 = false;
+
+function everyoneWasBornOnSecXX() {
+  // escreva seu código aqui
+  return books.every((book) => book.author.birthYear > 1900)
+}
+
+assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult3);
+
+
+// 6
+
+const expectedResult4 = true;
+
+function someBookWasReleaseOnThe80s() {
+  // escreva seu código aqui
+  return books.some((book) => book.releaseYear > 1980 && book.releaseYear < 1989)
+}
+
+assert.strictEqual(someBookWasReleaseOnThe80s(), expectedResult4);
+
+// 7 
+
+const expectedResult5 = false;
+
+function authorUnique() {
+  return books.every((book) => 
+    !books.some((bookSome) =>
+    (bookSome.author.birthYear === book.author.birthYear)
+    && (bookSome.author.name !== book.author.name)));
+}
+console.log(authorUnique())
+assert.strictEqual(authorUnique(), expectedResult5);
+
+
