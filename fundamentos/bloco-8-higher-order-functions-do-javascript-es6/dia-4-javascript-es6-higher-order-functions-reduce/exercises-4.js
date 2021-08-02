1aecc03c1231d6165c2527db00fb8003b067738c
Luiz Gustavo <gusstavopdm@gmail.com>
2021-08-02 20:16:20 -0300

    Concluido Exercicios - 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercises-4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercises-4.js
@@ -1,2 +1,34 @@
 const books = require('./data');
 const assert = require('assert');
+
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  // escreva seu código aqui
+  let nameOfBook = '';
+  const lengthBooks = books.reduce((acc, value) => {
+    if(acc < value.name.length) {
+      acc = value.name.length;
+      nameOfBook = value;
+    }
+    return acc;
+  
+  }, 0)
+
+  return nameOfBook;
+}
+
+// console.log(longestNamedBook())
+
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
+
