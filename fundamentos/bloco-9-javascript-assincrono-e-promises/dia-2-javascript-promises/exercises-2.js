736c75d9e14a1b5723f088f56b01791697cd94f8
Luiz Gustavo <gusstavopdm@gmail.com>
2021-08-11 18:45:17 -0300

    Conclusao Exercicios 3/4/5

--- fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/exercises-2.js
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/exercises-2.js
@@ -1,17 +1,35 @@
+
 const generateNumber = () => {
-  const randomNumber = Math.round((Math.random() * 50) + 1)
-  return Math.pow(randomNumber, 2)
+  const randomNumber = () => Math.round((Math.random() * 50) + 1)
+
+  const numbers = Array.from( 
+    { length: 10 }, 
+    () => Math.pow(randomNumber(), 2)
+  );
+  
+  const sum = numbers.reduce((acc, value) => acc + value, 0)
+  
+  if (sum >= 8000) {
+    throw new Error();
+  }
+  return sum;
 }
 
-const promise = () => {
-  const newPromise = new Promise((resolve, rejects) => {
-    const numbers = Array.from( {length: 10}, generateNumber);
-    const sumNumbers = numbers.reduce((acc, value) => acc + value, 0)
-    (sumNumbers < 8000) ? resolve() : rejects()
-  })
-  newPromise
-  .then(() => console.log('Promise resolvida'))
-  .catch(() => console.log('Promise rejeitada'));
+const divisionNumber = (sumNumbers) => {
+  const division = [2, 3, 5, 10].map(number => sumNumbers / number);
+  return division.reduce((acc, number) => acc + number, 0)
+}
+
+
+const promise = async () => {
+
+  try  {
+    const sumNumbers = await generateNumber();
+    const division = await divisionNumber(sumNumbers);
+    console.log(Math.round(division));
+  } catch (error) {
+    console.log("É mais de oito mil! Essa promise deve estar quebrada!");
+  }
 } 
 
 promise();
