c5db4cd1f8e41bbe249a806a6223ab635783818a
Luiz Gustavo <gusstavopdm@gmail.com>
2021-08-17 19:45:41 -0300

    Impletacao testes da func-myRemoveWithoutCopy && func-myFizzBuzz

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/Parte-I/test/myFizzBuzz.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/Parte-I/test/myFizzBuzz.test.js
@@ -1,1 +1,28 @@
 const myFizzBuzz = require('../src/myFizzBuzz')
+
+describe('myFizzBuzz', () => {
+  test('Funcao existente', () => {
+    expect(typeof myFizzBuzz).toEqual('function')
+  });
+
+  test('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(15)).toEqual('fizzbuzz')
+  });
+
+  test('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(9)).toEqual('fizz')
+  });
+
+  test('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(25)).toEqual('buzz')
+  });
+
+  test('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(7)).toBe(7)
+  });
+
+  test('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz('7')).toBeFalsy()
+  });
+});
+
