b2042b9e9a826464169860fb8f508c2e20796f9b
Luiz Gustavo <gusstavopdm@gmail.com>
2021-08-17 19:49:25 -0300

    Impletacao testes da func-compareObj

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/Parte-I/test/compareObj.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-1-primeiros-passos-no-jest/Parte-I/test/compareObj.test.js
@@ -1,1 +1,15 @@
 const {obj1, obj2, obj3} = require('../src/compareObj');
+
+describe('compareObjeto', () => {
+  test('Compare dois objetos para verificar se são idênticos ou não (Obj1 e Obje2)', () => {
+    expect(obj1).toEqual(obj2)
+  });
+
+  test('Compare dois objetos para verificar se são idênticos ou não (Obj1 e Obje3)', () => {
+    expect(obj1).not.toEqual(obj3)
+  });
+
+  test('Compare dois objetos para verificar se são idênticos ou não (Obj2 e Obje3)', () => {
+    expect(obj2).not.toEqual(obj3)
+  });
+})
