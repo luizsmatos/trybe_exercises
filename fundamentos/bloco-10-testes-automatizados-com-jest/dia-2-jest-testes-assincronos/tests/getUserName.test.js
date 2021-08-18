6f0f55b24b7d0ff70e3c40ae6f2cf48cbb0fc55f
Luiz Gustavo <gusstavopdm@gmail.com>
2021-08-18 19:53:24 -0300

    Exercicio - 3

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/tests/getUserName.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/tests/getUserName.test.js
@@ -1,24 +1,46 @@
 const getUserName = require('../src/getUserName');
 
 
-describe('Testar funcao getUserName', () => {
+describe('Testar funcao getUserName - promise', () => {
 
-  it('Testa se o usuario Mark existe!', () => {
+  it('Teste se o usuario Mark existe!', () => {
     return getUserName(1).then((user) => {
       expect(user).toBe('Mark')
     })
   })
 
-  it('Testa se o usuario Paul existe!', () => {
+  it('Teste se o usuario Paul existe!', () => {
     return getUserName(2).then((user) => {
       expect(user).toBe('Paul')
     })
   })
 
-  it('Testa se o usuario Paul existe!', () => {
+  it('Teste se a funcao lanca um erro, quando nao encontra o usuario!!', () => {
     return getUserName(3).catch((error) => {
       expect.assertions(1);
       expect(error.message).toMatch('User with 3 not found.')
     })
+  })
+})
+
+describe('Testar funcao getUserName - async/await', () => {
+  it('Teste se o usuario Mark existe!', async () => {
+    const user = await getUserName(1);
+    expect(user).toBe('Mark');
+  })
+
+  it('Teste se o usuario Paul existe!', async () => {
+    const user = await getUserName(2);
+    expect(user).toBe('Paul');
+  })
+
+  it('Teste se a funcao lanca um erro, quando nao encontra o usuario!!', async () => {
+      expect.assertions(1);
+      try {
+        await getUserName(3)
+      } catch (error) {
+        expect(error.message).toMatch('User with 3 not found.')
+      }
+      
   })
 })
