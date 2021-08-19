afd7110dacfc60b8416971a7b60061aef74ecca8
Luiz Gustavo <gusstavopdm@gmail.com>
2021-08-19 20:19:53 -0300

    Exercicio - 2

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/tests/generateNumber.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/tests/generateNumber.test.js
@@ -1,12 +1,24 @@
 const math = require('../src/generateNumber')
 
+// jest.mock('../src/generateNumber')
+
 describe('Quando chamar a funcao generateNumber', () => {
 
-  it('Gera um numero', () => {
+  it('gere um número aleatório entre 0 e 100', () => {
     math.generateNumber = jest.fn().mockReturnValue(10)
 
     expect(math.generateNumber()).toBe(10);
     expect(math.generateNumber).toHaveBeenCalled();
     expect(math.generateNumber).toHaveBeenCalledTimes(1)
   });
+
+  it('uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo', () => {
+    
+    math.generateNumber = jest.fn().mockImplementation((a, b) => a / b);
+
+    expect(math.generateNumber(10, 5)).toBe(2);
+    expect(math.generateNumber).toHaveBeenCalled();
+    expect(math.generateNumber).toHaveBeenCalledTimes(1);
+    expect(math.generateNumber).toHaveBeenCalledWith(10, 5);
+  })
 });
