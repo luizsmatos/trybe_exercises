8b133bc66279ee01394cad9ba677de4ba207787b
Luiz Gustavo <gusstavopdm@gmail.com>
2021-08-19 20:35:13 -0300

    Exercicio - 3

--- fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/tests/generateNumber.test.js
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/tests/generateNumber.test.js
@@ -1,6 +1,5 @@
 const math = require('../src/generateNumber')
 
-// jest.mock('../src/generateNumber')
 
 describe('Quando chamar a funcao generateNumber', () => {
 
@@ -12,7 +11,7 @@ describe('Quando chamar a funcao generateNumber', () => {
     expect(math.generateNumber).toHaveBeenCalledTimes(1)
   });
 
-  it('uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo', () => {
+  it('uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo.', () => {
     
     math.generateNumber = jest.fn().mockImplementation((a, b) => a / b);
 
@@ -21,4 +20,23 @@ describe('Quando chamar a funcao generateNumber', () => {
     expect(math.generateNumber).toHaveBeenCalledTimes(1);
     expect(math.generateNumber).toHaveBeenCalledWith(10, 5);
   })
+
+  it('uma nova implementação que receba três parâmetros e retorne sua multiplicação, e depois, uma nova implementação que receba um parâmetro e retorne seu dobro', () => {
+    
+    math.generateNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+    expect(math.generateNumber(10, 5, 2)).toBe(100);
+    expect(math.generateNumber).toHaveBeenCalled();
+    expect(math.generateNumber).toHaveBeenCalledTimes(1);
+    expect(math.generateNumber).toHaveBeenCalledWith(10, 5, 2);
+
+    
+    math.generateNumber.mockRestore();
+    math.generateNumber = jest.fn().mockImplementation((a) => a * 2);
+  
+    expect(math.generateNumber(10)).toBe(20);
+    expect(math.generateNumber).toHaveBeenCalled();
+    expect(math.generateNumber).toHaveBeenCalledTimes(1);
+    expect(math.generateNumber).toHaveBeenCalledWith(10);
+  });
 });
